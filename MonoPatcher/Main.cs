2e569f70340d8e31c1dacb2a519416f86403d891
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-11-05 17:36:16 -0300

    Initial commit

---
+++ MonoPatcher/Main.cs
@@ -0,0 +1,16 @@
+using System;
+using Sims3.Gameplay.Utilities;
+using Sims3.SimIFace;
+
+namespace MonoPatcher
+{
+	internal class Main
+	{
+		[Tunable] static bool init;
+		
+		static Main()
+		{
+
+		}
+	}
+}
---
+++ MonoPatcher/ReflectionUtility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace MonoPatcher
+{
+    public static class ReflectionUtility
+    {
+        public static MethodInfo GetMethod(string methodName, Type type)
+        {
+            var methods = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+            foreach (var method in methods)
+            {
+                if (method.Name == methodName)
+                    return method;
+            }
+            return null;
+        }
+
+    }
+}
