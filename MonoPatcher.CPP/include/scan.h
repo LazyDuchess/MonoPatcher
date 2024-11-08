fb5d8c82f3f8e22a286263f1ff33236c34b9337f
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-11-07 23:36:40 -0300

    add some more things to scan

--- MonoPatcher.CPP/include/scan.h
+++ MonoPatcher.CPP/include/scan.h
@@ -1,4 +1,7 @@
 #pragma once
 
+void MakeJMP(BYTE* pAddress, DWORD dwJumpTo, DWORD dwLen);
+void Nop(BYTE* pAddress, DWORD dwLen);
+void WriteToMemory(DWORD addressToWrite, void* valueToWrite, int byteNum);
 char* ScanBasic(char* pattern, char* mask, char* begin, int size);
 char* ScanInternal(char* pattern, char* mask, char* begin, int size);
